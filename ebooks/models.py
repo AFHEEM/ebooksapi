5ed73c606439f972e64d116f2322410c19beec7d
C57687 <muhammad.faheem@nl.abnamro.com>
2021-06-07 13:10:54 +0200

    Created a Review API with ListCreate, Create and Retrieve API View

--- ebooks/models.py
+++ ebooks/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+class Ebook(models.Model):
+    """
+    Ebook database table
+    """
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    publication_date = models.DateField()
+
+    def __str__(self):
+        """
+        Represents the database objects display name
+        :return:
+        """
+        return self.title
+
+
+class Review(models.Model):
+    """
+    Review an Ebook
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    review_author = models.CharField(max_length=8, blank=True, null=True)
+    review = models.TextField(blank=True, null=True)
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1),
+                                                     MaxValueValidator(5)])
+    ebook = models.ForeignKey(Ebook,
+                              on_delete=models.CASCADE,
+                              related_name='reviews')
+
+    def __str__(self):
+        """
+        Review model representation
+        :return:
+        """
+        return str(self.rating)
--- ebooks/views.py
+++ ebooks/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+# from rest_framework import mixins
+from rest_framework import generics
+from ebooks.models import Ebook, Review
+from ebooks.serializers import EbookSerializer, ReviewSerializer
 
-# Create your views here.
+
+class EbookListCreateAPIView(generics.ListCreateAPIView):
+    """
+    List Ebooks
+    """
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    List Ebooks
+    """
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class ReviewCreateAPIView(generics.CreateAPIView):
+    """
+    Create a Review
+    """
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        """
+        Override Review creation
+        :param serializer:
+        :return:
+        """
+        ebook_pk = self.kwargs.get('ebook_pk')
+        ebook = generics.get_object_or_404(Ebook, pk=ebook_pk)
+        serializer.save(ebook=ebook)
+
+
+class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Get, update and delete a review object
+    """
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+# class EbookListCreateAPIView(mixins.ListModelMixin,
+#                              mixins.CreateModelMixin,
+#                              generics.GenericAPIView):
+#     """
+#     Display Ebooks as a list
+#     """
+#     queryset = Ebook.objects.all()
+#     serializer_class = EbookSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         """
+#         Get all ebooks
+#         :param request:
+#         :return:
+#         """
+#         return self.list(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         """
+#         Get all ebooks
+#         :param request:
+#         :return:
+#         """
+#         return self.create(request, *args, **kwargs)
